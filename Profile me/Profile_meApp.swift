5ee1fb1a5d2c325c96146139bfbca2cf9644950f
razanalsaad <147040760+razanalsaad@users.noreply.github.com>
2023-10-09 01:29:45 +0300

    Initial Commit

---
+++ Profile me/Profile_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_meApp.swift
+//  Profile me
+//
+//  Created by razan on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Profile_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
